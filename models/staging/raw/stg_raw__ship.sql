79fa62edb7763e296ea0f124f326c71dcc5858cd
Azania "Prince" Mzolisa <ypmzolisa@gmail.com>
2025-02-11 10:40:05 +0000

    ship model updated

---
+++ models/staging/raw/stg_raw__ship.sql
@@ -0,0 +1,16 @@
+with 
+
+source as (
+    select * from {{ source('raw', 'ship') }}
+),
+
+renamed as (
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        cast(ship_cost as INT64) AS ship_cost
+    from source
+)
+
+select * from renamed
